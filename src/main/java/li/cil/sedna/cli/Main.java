80bab0fa06c54600227771f1913eb2bd59b942d9
lavender hearts <hearts@colbatic.com>
2025-12-24 14:24:30 -0500

    spike: Initial usage of OpenComputers 2: Reimagined RPC Device API

--- src/main/java/li/cil/sedna/cli/Main.java
+++ src/main/java/li/cil/sedna/cli/Main.java
@@ -21,6 +21,22 @@ import org.jline.terminal.TerminalBuilder;
 import org.jline.terminal.impl.DumbTerminal;
 import org.jline.utils.NonBlockingReader;
 
+import li.cil.oc2.api.bus.device.Device;
+import li.cil.oc2.api.bus.device.object.Callback;
+import li.cil.oc2.api.bus.device.object.NamedDevice;
+import li.cil.oc2.api.bus.device.object.ObjectDevice;
+import li.cil.oc2.api.bus.device.rpc.RPCDevice;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.SubscriberExceptionContext;
+
+import li.cil.oc2.common.bus.RPCDeviceBusAdapter;
+import li.cil.oc2.common.vm.context.global.GlobalVMContext;
+import li.cil.oc2.common.vm.BuiltinDevices;
+import li.cil.oc2.common.vm.VMDeviceBusAdapter;
+
+
+
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
@@ -35,26 +51,28 @@ public final class Main {
         final List<String> argList = Arrays.asList(args);
         final boolean enableGdbStub = argList.contains("-s");
         final boolean waitForGdb = argList.contains("-S");
-        final boolean runBenchmark = argList.contains("--benchmark");
 
-        if (runBenchmark) {
-            runBenchmark();
-        } else {
-            runSimple(enableGdbStub, waitForGdb);
-        }
+        runSimple(enableGdbStub, waitForGdb);
     }
 
     private static void runSimple(final boolean enableGdbStub, final boolean waitForGdb) throws Exception {
         final R5Board board = new R5Board();
         final PhysicalMemory memory = Memory.create(32 * 1024 * 1024);
-        final UART16550A uart = new UART16550A();
+        //final UART16550A uart = new UART16550A();
         final GoldfishRTC rtc = new GoldfishRTC(SystemTimeRealTimeCounter.get());
 
+		final GlobalVMContext context = new GlobalVMContext(board);
+		final BuiltinDevices builtinDevices;
+	   	builtinDevices = new BuiltinDevices(context);
+		final RPCDeviceBusAdapter rpcAdapter = new RPCDeviceBusAdapter(builtinDevices.rpcSerialDevice);
+		final VMDeviceBusAdapter vmAdapter;
+	   	vmAdapter = new VMDeviceBusAdapter(context);
+	
 		// terminal signals
 		SignalHandler handler = new SignalHandler () {
 			// ^C
    			public void handle(Signal sig) {
-			 uart.putByte((byte) 0x03);
+			 builtinDevices.uart.putByte((byte) 0x03);
 		    }
 		};
 		
@@ -67,19 +85,20 @@ public final class Main {
         final VirtIOBlockDevice hdd = new VirtIOBlockDevice(board.getMemoryMap(), rootfs);
         final VirtIOFileSystemDevice fs = new VirtIOFileSystemDevice(board.getMemoryMap(), "host_fs", new HostFileSystem());
 
-        uart.getInterrupt().set(0xA, board.getInterruptController());
+        builtinDevices.uart.getInterrupt().set(0xA, board.getInterruptController());
         rtc.getInterrupt().set(0xB, board.getInterruptController());
         hdd.getInterrupt().set(0x1, board.getInterruptController());
         fs.getInterrupt().set(0x2, board.getInterruptController());
 
         board.addDevice(0x80000000L, memory);
-        board.addDevice(uart);
+        board.addDevice(builtinDevices.uart);
         board.addDevice(rtc);
         board.addDevice(hdd);
         board.addDevice(fs);
 
+		board.getCpu().setFrequency(25_000_000);
         board.setBootArguments("root=/dev/vda rw");
-        board.setStandardOutputDevice(uart);
+        board.setStandardOutputDevice(builtinDevices.uart);
 
         board.reset();
 
@@ -109,12 +128,12 @@ public final class Main {
                     remaining -= cyclesPerStep;
 
                     int value;
-                    while ((value = uart.read()) != -1) {
+                    while ((value = builtinDevices.uart.read()) != -1) {
                         System.out.print((char) value);
                     }
 
-                    while (br.ready() && uart.canPutByte()) {
-                        uart.putByte((byte) br.read());
+                    while (br.ready() && builtinDevices.uart.canPutByte()) {
+                        builtinDevices.uart.putByte((byte) br.read());
                     }
                 }
 
@@ -134,99 +153,6 @@ public final class Main {
                 }
             }
         }
-    }
-
-    private static void runBenchmark() throws Exception {
-        final Images images = getImages();
-
-        final R5Board board = new R5Board();
-        final PhysicalMemory memory = Memory.create(20 * 1024 * 1024);
-        final UART16550A uart = new UART16550A();
-        final GoldfishRTC rtc = new GoldfishRTC(SystemTimeRealTimeCounter.get());
-        final VirtIOBlockDevice hdd = new VirtIOBlockDevice(board.getMemoryMap(),
-                ByteBufferBlockDevice.createFromStream(images.rootfs(), true));
-
-        uart.getInterrupt().set(0xA, board.getInterruptController());
-        rtc.getInterrupt().set(0xB, board.getInterruptController());
-        hdd.getInterrupt().set(0x1, board.getInterruptController());
-
-        board.addDevice(0x80000000L, memory);
-        board.addDevice(uart);
-        board.addDevice(rtc);
-        board.addDevice(hdd);
-
-        board.setBootArguments("root=/dev/vda ro");
-        board.setStandardOutputDevice(uart);
-
-        System.out.println("Waiting for profiler...");
-        Thread.sleep(5 * 1000);
-        System.out.println("Initializing...");
-
-        final long cyclesPerRun = 300_000_000;
-        final int cyclesPerStep = 1_000;
-        final int hz = board.getCpu().getFrequency();
-
-        final int samples = 10;
-        int minRunDuration = Integer.MAX_VALUE;
-        int maxRunDuration = Integer.MIN_VALUE;
-        int accRunDuration = 0;
-
-        final R5CPU cpu = board.getCpu();
-        final StringBuilder sb = new StringBuilder(16 * 1024);
-
-        System.out.println("Starting...");
-
-        for (int i = 0; i < samples; i++) {
-            board.reset();
-
-            for (int offset = 0; offset < memory.getLength(); offset += 4) {
-                memory.store(offset, 0, Sizes.SIZE_32_LOG2);
-            }
-
-            loadProgramFile(memory, images.firmware());
-            loadProgramFile(memory, images.kernel(), 0x200000);
-
-            board.initialize();
-            board.setRunning(true);
-
-            sb.setLength(0);
-
-            final long runStart = System.currentTimeMillis();
-
-            final long limit = cpu.getTime() + cyclesPerRun;
-            int remaining = 0;
-            while (cpu.getTime() < limit && board.isRunning()) {
-                remaining += hz;
-                while (remaining > 0) {
-                    board.step(cyclesPerStep);
-                    remaining -= cyclesPerStep;
-
-                    int value;
-                    while ((value = uart.read()) != -1) {
-                        sb.append((char) value);
-                    }
-                }
-            }
-
-            final int runDuration = (int) (System.currentTimeMillis() - runStart);
-            accRunDuration += runDuration;
-            minRunDuration = Integer.min(minRunDuration, runDuration);
-            maxRunDuration = Integer.max(maxRunDuration, runDuration);
-
-            System.out.print(sb);
-
-            System.out.printf("\n\ntime: %.2fs\n", runDuration / 1000.0);
-        }
-
-        final int avgDuration = accRunDuration / samples;
-        System.out.printf("\n\ntimes: min=%.2fs, max=%.2fs, avg=%.2fs\n",
-                minRunDuration / 1000.0,
-                maxRunDuration / 1000.0,
-                avgDuration / 1000.0);
-        System.out.printf("\n\nhz: max=%.2fsMHz, min=%.2fMHz, avg=%.2fMHz\n",
-                (cyclesPerRun / 1_000_000.0) / (minRunDuration / 1000.0),
-                (cyclesPerRun / 1_000_000.0) / (maxRunDuration / 1000.0),
-                (cyclesPerRun / 1_000_000.0) / (avgDuration / 1000.0));
     }
 
     private static void loadProgramFile(final PhysicalMemory memory, final InputStream stream) throws Exception {
